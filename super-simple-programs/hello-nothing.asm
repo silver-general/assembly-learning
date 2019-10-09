30d5eb7e82c55af51900d40050f919cfe58c1be3
Morgi_93 <56215982+silver-general@users.noreply.github.com>
2019-10-09 10:14:53 +0200

    Update hello-nothing.asm

--- super-simple-programs/hello-nothing.asm
+++ super-simple-programs/hello-nothing.asm
@@ -1,5 +1,9 @@
 // the most basic program: do nothing (use a forever loop)
 
-start:
-  NOP
+start:        ; start is a label. it substitutes the address where the program counter starts counting instructions.
+  NOP         ; does nothing. 1 clock cycle.
+  
+// how heavy is this program? 
+// every instruction takes 16 bits -> 2 bytes?
+
 
