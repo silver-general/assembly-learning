7fbe00386e7329b19110551149d44d24a8114892
Morgi_93 <56215982+silver-general@users.noreply.github.com>
2019-10-11 13:11:50 +0200

    Update first-program-structure.asm

--- super-simple-programs/first-program-structure.asm
+++ super-simple-programs/first-program-structure.asm
@@ -2,8 +2,8 @@
 .include "tn85def.inc" ; Define device ATtiny85 keywords
 .list
 
-.cseg         ; code segment: cosa fa di preciso?
-.org 000000   ; sets start of program to address 0 of program memory (in SRAM)
+.cseg         ; code segment: tells the assembler to write the following in the Program Memory (flash ROM)
+.org 000000   ; sets start of program to address 0 of program memory (in flash ROM)
 
 Main:
 Loop:
